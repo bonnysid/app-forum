fed05506d681940993376811c59b8597d5f6c7d3
bonnysid <bonnysidworker@gmail.com>
2020-09-12 22:38:30 +0400

    add new files

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app-header.css';
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Nikita Bortsov</h1>
+            <h2>Posts: 5; Likes: 1</h2>
+        </div>
+    );
+}
+
+export default AppHeader;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/app/';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class WhoAmI extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    state = {
+        age: 24
+    }
+
+    nextYear = () => {
+        this.setState(state => ({
+            age: ++state.age
+        }))
+    }
+
+    render() {
+        const {name, surname, link} = this.props;
+        const {age} = this.state;
+        return (
+            <>
+                <button onClick={this.nextYear}>++</button>
+                <h1>My name is {name}, surname is {surname}, my age is {age};</h1>
+                <a href={link}>My profile</a>
+             </>
+        );
+    }
+}   
+
+const All = () => {
+    return (
+        <>
+            <WhoAmI name = "Jhon" surname = "Smith" link = "vk.com"/>
+            <WhoAmI name = "Alex" surname = "Smith" link = "vk.com"/>
+            <WhoAmI name = "Max" surname = "Smith" link = "vk.com"/>
+        </>
+    );
+}
+ReactDOM.render(<All />, document.getElementById('root'));
+
